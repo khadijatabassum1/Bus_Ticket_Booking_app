55a43842331a768c1f5080a1a7df59b6decf93cf
khadijatabussum1 <khadijatabussum270@gmail.com>
2018-12-22 09:14:16 +0500

    Initial commit

---
+++ Bus-Ticket-app-master/app/src/main/java/com/kt/app/busticket/Constants.java
@@ -0,0 +1,10 @@
+package com.kt.app.busticket;
+
+/**
+ * Created by kt on 8/10/18.
+ */
+
+public class Constants {
+    public static final String HAS_BOOKED = "has_booked";
+
+}
---
+++ Bus-Ticket-app-master/app/src/main/java/com/kt/app/busticket/provider/base/BaseModel.java
@@ -0,0 +1,4 @@
+package com.kt.app.busticket.provider.base;
+
+public interface BaseModel {
+}
